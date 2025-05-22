b499c3ad956cc9d862806fd7d59c86228d74f6ce
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 22:37:12 +0200

    Testfall 7: 3. Step refactor -> Code verbessert durch Herausheben von Variablen

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -2,11 +2,15 @@
 function findSpaceship() {}
 findSpaceship.prototype.add = function(map) {
 	let result = "Spaceship lost forever";
+
 	const lines = map.split('\n');
+	const highestLine = lines.length - 1;
+
 	lines.forEach(line => {
 		if (line.includes('X')) {
 			const xCoordinate = line.indexOf('X');
-			result = [xCoordinate, (lines.length-1) - lines.indexOf(line)]; //y reversed
+			const yCoordinate = highestLine - lines.indexOf(line); //y reversed
+			result = [xCoordinate, yCoordinate];
 		}
 	});
 	return result;
