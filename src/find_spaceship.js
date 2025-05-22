9169dfc63dc96fcd7b4dc3584274c0b883e5831a
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 22:18:36 +0200

    Testfall 5: 3. Step refactor -> Code verschönern, durch Herausheben des Ergebnisses und Einfügen von if/else-if

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -1,12 +1,13 @@
 // FindSpaceship.js
 function findSpaceship() {}
 findSpaceship.prototype.add = function(map) {
+	let result = "Spaceship lost forever";
 	const lines = map.split('\n');
 	if (lines[lines.length-1].includes('.X')) {
-		return [1,0];
+		result = [1,0];
 	}
-	if (map.includes('X')) {
-		return [0,0];
+	else if (map.includes('X')) {
+		result = [0,0];
 	}
-	return "Spaceship lost forever";
+	return result;
 };
