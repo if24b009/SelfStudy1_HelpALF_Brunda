2978fd31de9520f46823f8cfd0fc79f193e08a80
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 21:58:57 +0200

    Testfall 1: 1. step red -> fehlerhafter testfall für leerstring geschrieben

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -3,7 +3,7 @@ describe('Find Spaceship', function() {
 		spaceshipFinder = new findSpaceship();
 	});
 
-	it('should return 0 for an empty string', function() {
-		expect(spaceshipFinder.add('')).toEqual(0);
+	it('should return "Spaceship lost forever" for an empty string', function() {
+		expect(spaceshipFinder.add('')).toEqual("Spaceship lost forever");
 	});
 });
