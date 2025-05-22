5e9fd7ba58a564dec56d7426bc1dd6f193dda249
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 22:16:57 +0200

    Testfall 5: 2. Step green -> durch splitten an \n und einfachste Überprüfung auf .X wird Test erfolgreich

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -1,6 +1,10 @@
 // FindSpaceship.js
 function findSpaceship() {}
 findSpaceship.prototype.add = function(map) {
+	const lines = map.split('\n');
+	if (lines[lines.length-1].includes('.X')) {
+		return [1,0];
+	}
 	if (map.includes('X')) {
 		return [0,0];
 	}
