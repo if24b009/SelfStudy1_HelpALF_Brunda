39fdba338a826c74ed8dc590730a9f9ec9af754c
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 22:05:55 +0200

    Testfall 4: 2. Step green -> simples if auf Überprüfung ob X in map ist erzielt erfolgreichen Test

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -1,5 +1,8 @@
 // FindSpaceship.js
 function findSpaceship() {}
 findSpaceship.prototype.add = function(map) {
+	if (map.includes('X')) {
+		return [0,0];
+	}
 	return "Spaceship lost forever";
 };
