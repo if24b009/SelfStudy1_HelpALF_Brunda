10737cf1b4f990ac578e1c37e13dc80665e78eab
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-22 22:02:46 +0200

    Testfall 3: 1. & 2. step red & green -> wie bei test2 ist dieser test mit map ohne X aufgrund des returns aus test1 erfolgreich

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -10,4 +10,8 @@ describe('Find Spaceship', function() {
 	it('should return "Spaceship lost forever" for "........."', function() {
 		expect(spaceshipFinder.add('.........')).toEqual("Spaceship lost forever");
 	});
+
+	it('should return "Spaceship lost forever" for "..........\n..........\n..........\n..........\n..........\n.........."', function() {
+		expect(spaceshipFinder.add('..........\n..........\n..........\n..........\n..........\n..........')).toEqual("Spaceship lost forever");
+	});
 });
